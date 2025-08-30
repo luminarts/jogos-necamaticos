8bdc64ab35f372dfeb84a4f697872a991c8edbed
Gustavo Borba <gustavo.borba2001@gmail.com>
2025-08-30 19:53:24 -0300

    feat: blink implementation partial

--- src/app/pisca/page.js
+++ src/app/pisca/page.js
@@ -8,6 +8,7 @@ export default function Pisca() {
   - função de checar pra ver se arrays são iguais
   - checar a cada clique se equivale ao quadrado 
   - fazer um useEffect pra variavel os eligibleIds pra quando ela mudar, redistribuir os Ids entre os botões (deixar independente do tamanho do grid)
+  - botão para começar o blink
   */
 
   const [show4x4Grid, enable4x4Grid] = useState(false);
@@ -15,6 +16,7 @@ export default function Pisca() {
   const [currentLevel, setCurrentLevel] = useState(1);
   const [userBlinkOrder, setUserBlinkOrder] = useState([]);
   const [correctBlinkOrder, setCorrectBlinkOrder] = useState([1, 2]);
+  const [showCorrectOrder, setShowCorrectOrder] = useState(false);
 
   var eligibleIds = [];
   var randAmountOfBlinks = 0;
@@ -22,6 +24,13 @@ export default function Pisca() {
 
   // uma iteração a cada mudança de level
   useEffect(() => {
+    if(currentLevel > 3) {
+      enable4x4Grid(true);
+    }
+    if(currentLevel > 7) {
+      enable5x5Grid(true);
+    }
+    
     eligibleIds = enable4x4Grid? (enable5x5Grid? Array.from({ length: 25 }, (_, i) => i + 1) : Array.from({ length: 16 }, (_, i) => i + 1)): Array.from({ length: 4 }, (_, i) => i + 1);
     
     randAmountOfBlinks = Math.floor(Math.random() * eligibleIds.length);
@@ -32,13 +41,9 @@ export default function Pisca() {
     setCorrectBlinkOrder(auxBlinkOrder);
     auxBlinkOrder = []
     
-    if(currentLevel > 3) {
-      enable4x4Grid(true);
-    }
-    if(currentLevel > 7) {
-      enable5x5Grid(true);
-    }
+
   },[currentLevel]);
+
 
   // Checar se cada número tá certo quando clicado
   useEffect(() => {
@@ -48,18 +53,24 @@ export default function Pisca() {
   }, [userBlinkOrder])
   
 
-  for (var i = 0; i < eligibleIds.length; i++) {
+  // for (var i = 0; i < eligibleIds.length; i++) {
 
-  }
+  // }
+
+  // Fazer mostrar a ordem certa
+
+  useEffect(() => {
+    
+  }, [showCorrectOrder]);
 
   return (
 <div className="flex flex-col justify-center items-center">
-  <div className="h-screen flex flex-col items-center justify-center">
+  <div className="h-screen flex flex-row items-center justify-center">
     <div className="grid grid-cols-5 gap-4">
       <PiscaButton buttonId={1} userBlinkOrder={userBlinkOrder}></PiscaButton>
       <PiscaButton buttonId={2} userBlinkOrder={userBlinkOrder}></PiscaButton>
-      <PiscaButton buttonId={5} userBlinkOrder={userBlinkOrder}></PiscaButton>
-      <PiscaButton buttonId={6} userBlinkOrder={userBlinkOrder}></PiscaButton>
+      {show4x4Grid && <PiscaButton buttonId={5} userBlinkOrder={userBlinkOrder}></PiscaButton>}
+      {show4x4Grid && <PiscaButton buttonId={6} userBlinkOrder={userBlinkOrder}></PiscaButton>}
       <PiscaButton buttonId={17} userBlinkOrder={userBlinkOrder}></PiscaButton>
       <PiscaButton buttonId={3} userBlinkOrder={userBlinkOrder}></PiscaButton>
       <PiscaButton buttonId={4} userBlinkOrder={userBlinkOrder}></PiscaButton>
@@ -82,6 +93,7 @@ export default function Pisca() {
       <PiscaButton buttonId={24} userBlinkOrder={userBlinkOrder}></PiscaButton>
       <PiscaButton buttonId={25} userBlinkOrder={userBlinkOrder}></PiscaButton>
     </div>
+    <button className="p-2 border-2 mx-4 hover:bg-gray-200 ">Mostrar ordem correta</button>
   </div>
 </div>
   );
