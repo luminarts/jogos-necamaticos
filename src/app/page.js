1bb956f698ab29e62002c9358de46779f59e87cb
Gustavo Borba <gustavo.borba2001@gmail.com>
2025-09-09 21:54:48 -0300

    feat: alinhamento dos botões

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,12 @@ import RoutingButton from "./client_components/routingButton";
 
 export default function Home() {
   return (
-  <div className="h-screen flex flex-col items-center justify-center dark:bg-black dark:text-white">
-    <div className="bg-black-500 p-8 text-black">
+  <div className="h-screen flex flex-col justify-center items-center dark:bg-black dark:text-white bg-white text-black">
+    <div className=" p-8 ">
       Escolha o jogo que deseja jogar:
     </div>
-    <div className="flex flex-row">
-      <div className="border-2 border-black-500 p-4 m-4">
+    <div className="flex justify-center items-center ">
+      <div className="border-2 border-black-500 p-4 m-4 flex flex-col sm:flex-row">
         <RoutingButton href={'/equacao'}>
           Complete a equação!
         </RoutingButton>
